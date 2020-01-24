9643e37ba542e97f1b8778f17257510028b6f974
Josh Doyle <me@joshdoyle.us>
2020-01-24 13:43:03 -0600

    added folder structure and shell files

---
+++ server.js
@@ -0,0 +1,45 @@
+require ('dotenv').config()
+const express = require('express')
+const server = express()
+const PORT = process.env.PORT
+const bodyParser = require('body-parser')
+const session = require('express-session')
+const methodOverride = require('method-override')
+
+// ------ DATABASE ------
+
+// connect to db
+require('./db/db') 
+
+// ------ MIDDLEWARE ------
+server.use(express.static('public')) 
+server.use(bodyParser.urlencoded({ extended: false }))
+server.use(methodOverride('_method'))
+
+// ------ SESSION ------
+server.use(session({
+	secret: process.env.SESSION_SECRET,
+	resave: false,
+	saveUninitialized: false
+}))
+
+// ------- CONTROLLERS -----
+const userController = require('./controllers/userController.js')
+server.use('/users', userController)
+
+const authController = require('./controllers/authController.js')
+server.use('/auth', authController)
+
+// ------ ROUTES ------
+server.get('/', (req, res) => {
+
+})
+
+server.get('*', (req, res) => {
+	res.status(404).send('404 page not found')
+})
+
+server.listen(PORT, () => {
+	const d = new Date()
+	console.log(`${d.toLocaleString()}: Server running on port ${PORT}.`)	
+})
